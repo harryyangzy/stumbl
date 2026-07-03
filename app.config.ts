b6bf545151e43f9f9b59be021369db543bfc2258
Harry Yang <hyang746@uwo.ca>
2026-07-03 17:16:40 -0400

    Bump iOS build number to 6 for TestFlight release.

--- app.config.ts
+++ app.config.ts
@@ -48,7 +48,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: 'ca.stumbl.app',
-    buildNumber: '5',
+    buildNumber: '6',
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       NSAppTransportSecurity: {
